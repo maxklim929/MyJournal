bccdcd0e6b5fa37faf3c519ac2c943e48f827dcf
maxklim929 <85760083+maxklim929@users.noreply.github.com>
2023-10-31 13:38:10 -0400

    local storage work

--- travel-track.js
+++ travel-track.js
@@ -2,32 +2,43 @@ function saveTravel(rest, city, desc) {
     let info = [];
     info.push(rest, city, desc);
     if (data == null) {
-      data = [info];
+        data = [info];
     } else {
-      data.push(info);
+        data.push(info);
     }
     console.log("Saved")
     localStorage.setItem('travelLog', JSON.stringify(data)); // update localStorage
     console.log(data);
 }
+
 let entries = localStorage.getItem('travelLog');
 var data = [];
 if (entries != '') {
-  data = JSON.parse(entries);
+    data = JSON.parse(entries);
+    for (let entry of data) {
+        document.getElementById("card-container").innerHTML +=
+        `<div class="card" style="width: 40rem; margin-top: 1rem;">
+    <img src="restaurant.jpeg" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${entry[0]}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${entry[1]}</h6>
+    <p class="card-text">${entry[2]}</p>
+    </div>
+    </div>`;
+    }
 }
+
 document.getElementById('submitbutton').addEventListener("click", () => {
     let restname = document.getElementById('restaurant-name').value;
     let city = document.getElementById('city').value;
     let desc = document.getElementById('text').value;
-    document.getElementById("card-container").innerHTML += 
-    `<div class="card" style="width: 40rem; margin-top: 1rem;">
+    document.getElementById("card-container").innerHTML +=
+        `<div class="card" style="width: 40rem; margin-top: 1rem;">
     <img src="restaurant.jpeg" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">${restname}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${city}</h6>
         <p class="card-text">${desc}</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
     </div>
     </div>`;
     console.log("Clicked");
