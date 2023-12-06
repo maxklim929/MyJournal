5fe9261d503018a2da1c9f635e84da4bdb48b060
maxklim929 <85760083+maxklim929@users.noreply.github.com>
2023-12-05 22:11:15 -0500

    Button switch

--- script/mylist.js
+++ script/mylist.js
@@ -1,42 +1,54 @@
 fetch(`https://uselessfacts.jsph.pl/api/v2/facts/today`).then(response => response.json()).then(data => {
     document.getElementById("define").innerText = data["text"];
     document.getElementById("save_fact").addEventListener('click', () => {
+        document.getElementById("save_fact").style.backgroundColor = 'white';
+        document.getElementById("save_fact").style.color = 'blue';
         if (localStorage.getItem('Facts') == null) {
             var new_data = [document.getElementById("define").innerText];
+            console.log(new_data);
             localStorage.setItem('Facts', JSON.stringify(new_data));
         } else {
             var curr_hist = JSON.parse(localStorage.getItem('Facts'));
             curr_hist.push(document.getElementById("define").innerText)
             localStorage.setItem('Facts', JSON.stringify(curr_hist));
             curr_hist = JSON.parse(localStorage.getItem('Facts'));
+            console.log(curr_hist);
         }
     });
 });
 fetch(`https://www.boredapi.com/api/activity/`).then(response => response.json()).then(data => {
     document.getElementById("bored").innerText = data["activity"];
     document.getElementById("save_bored").addEventListener('click', () => {
+        document.getElementById("save_bored").style.backgroundColor = 'white';
+        document.getElementById("save_bored").style.color = 'blue';
         if (localStorage.getItem('Bored') == null) {
             var new_data = [document.getElementById("bored").innerText];
+            console.log(new_data);
             localStorage.setItem('Bored', JSON.stringify(new_data));
         } else {
             var curr_hist = JSON.parse(localStorage.getItem('Bored'));
             curr_hist.push(document.getElementById("bored").innerText)
             localStorage.setItem('Bored', JSON.stringify(curr_hist));
             curr_hist = JSON.parse(localStorage.getItem('Bored'));
+            console.log(curr_hist);
         }
     });
 });
 fetch(`https://api.quotable.io/random`).then(response => response.json()).then(data => {
     document.getElementById("ins_quote").innerHTML = `${data["content"]} <br> <i>-${data["author"]}</i>`;
     document.getElementById("save_quote").addEventListener('click', () => {
+        document.getElementById("save_quote").style.backgroundColor = 'white';
+        document.getElementById("save_quote").style.color = 'blue';
         if (localStorage.getItem('Quote') == null) {
             var new_data = [document.getElementById("ins_quote").innerHTML];
+            console.log(new_data);
             localStorage.setItem('Quote', JSON.stringify(new_data));
         } else {
             var curr_hist = JSON.parse(localStorage.getItem('Quote'));
             curr_hist.push(document.getElementById("ins_quote").innerHTML)
             localStorage.setItem('Quote', JSON.stringify(curr_hist));
             curr_hist = JSON.parse(localStorage.getItem('Quote'));
+            console.log(curr_hist);
         }
     });
 });
@@ -61,6 +73,8 @@ fetch(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=H92OvzsBcMz4
                                                     <br> ${news_arr[1]["abstract"]}
                                                     <br> <a href="${news_arr[1]["url"]}">Link to Article</a>`;
     document.getElementById("save_c1").addEventListener('click', () => {
+        document.getElementById("save_c1").style.backgroundColor = 'white';
+        document.getElementById("save_c1").style.color = 'blue';
         if (localStorage.getItem('News') == null) {
             var new_data = [data["results"][c1_ind]];
             localStorage.setItem('News', JSON.stringify(new_data));
@@ -72,6 +86,8 @@ fetch(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=H92OvzsBcMz4
         }
     });
     document.getElementById("save_c2").addEventListener('click', () => {
+        document.getElementById("save_c2").style.backgroundColor = 'white';
+        document.getElementById("save_c2").style.color = 'blue';
         if (localStorage.getItem('News') == null) {
             var new_data = [data["results"][c1_ind]];
             localStorage.setItem('News', JSON.stringify(new_data));
@@ -80,6 +96,7 @@ fetch(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=H92OvzsBcMz4
             curr_hist.push(data["results"][c1_ind])
             localStorage.setItem('News', JSON.stringify(curr_hist));
             curr_hist = JSON.parse(localStorage.getItem('News'));
+            console.log(curr_hist);
         }
     });
 });
